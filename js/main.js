65e629a0e3a18e58d346da5c878d0625a309fe09
Rubab Akmal <rubabakmal19@gmail.com>
2022-03-13 16:51:53 +0500

    Healthy coach

---
+++ js/main.js
@@ -0,0 +1,38 @@
+jQuery(function ($) {
+    "use strict";
+
+    /* Window Load ---------------------- */
+
+    $(window).on('load', function () {
+
+
+    });
+
+
+    /* Document Ready ------------------- */
+
+    $(document).ready(function () {
+
+
+        AOS.init({
+            duration:2000
+        });
+    });
+
+
+    /* Window Scroll -------------------- */
+
+    $(window).on('scroll', function () {
+
+
+    });
+
+
+    /* Window Resize -------------------- */
+
+    $(window).on('resize', function () {
+
+
+    });
+
+});
